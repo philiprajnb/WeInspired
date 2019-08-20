9a9bb69def60bc68860dd79c37bd0f827aa6f126
philiprajnb <philiprajnb@gmail.com>
2019-08-21 01:14:36 +0530

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
   // Your location
-  const loc = { lat: 42.361145, lng: -71.057083 };
+  const loc = { lat: 27.2038, lng: 7.5011 };
   // Centered map on location
   const map = new google.maps.Map(document.querySelector('.map'), {
     zoom: 14,
